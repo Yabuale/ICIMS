76e0f9375daa284949e8b517a5398aceee277c57
Yeabsra Alebachew <yeabsraalebachew9@gmail.com>
2024-02-10 15:12:19 +0300

    api end-points for wanted criminal done

--- backend/wanted/views.py
+++ backend/wanted/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import WantedCriminal
+from .serializers import WantedSerializer
+from rest_framework.decorators import api_view
 
-# Create your views here.
+class WantedCriminalView(APIView):
+      def get(self, request):
+        criminals = WantedCriminal.objects.all()
+        serializer = WantedSerializer(criminals, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['POST'])
+def addWanted(request):
+    serializer = WantedSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"success": "Wanted criminal added successfully"} )
+    else:
+        return Response(serializer.errors, status=400)
+    
+
+    
