3b3393d4337cad8d2e955c2d5a3e6b227b5e926f
naolkuma <naolkuma02@gmail.com>
2024-02-12 22:26:08 +0300

    front-end started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,30 +4,38 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+    <nav className="w-full relative flex flex-wrap items-center justify-between px-2 py-6 bg-amber-500 mb-3">
+      <div className="w-full mx-auto flex flex-wrap items-center justify-between px-8">
+        <div className="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
+          <a className="text-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="#pablo">
+            ICMIS
+          </a>
+          <button className="cursor-pointer text-2xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button">
+            <span className="block relative w-8 h-px rounded-sm bg-white"></span>
+            <span className="block relative w-8 h-px rounded-sm bg-white mt-1"></span>
+            <span className="block relative w-8 h-px rounded-sm bg-white mt-1"></span>
+          </button>
+        </div>
+        <div className="lg:flex flex-grow items-center" id="example-navbar-warning">
+          <ul className="flex flex-col lg:flex-row list-none ml-auto">
+              <li className="nav-item">
+                <a className="px-3 py-2 flex items-center text-base uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+                  Wanted Criminal
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="px-3 py-2 flex items-center text-base uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+                  Login
+                </a>
+              </li>
+          </ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </nav>
     </>
   )
 }
