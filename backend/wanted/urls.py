48603e759ab3979b343de3e436de3c3060b896ee
Yeabsra Alebachew <yeabsraalebachew9@gmail.com>
2024-03-04 16:21:21 +0300

    wanted view, edit and delete endpoints

--- backend/wanted/urls.py
+++ backend/wanted/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import WantedCriminalView
+from .views import WantedCriminalView,WantedDetail
 from . import views
 
 urlpatterns = [
     path('', WantedCriminalView.as_view()),
+    path('detail/<int:pk>/', WantedDetail.as_view()),
     path('addWanted', views.addWanted),
 
 ]
