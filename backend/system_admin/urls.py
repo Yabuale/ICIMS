ce6ce6715be1614dcaa7f59634747ddaf5835ead
Yeabsra Alebachew <yeabsraalebachew9@gmail.com>
2024-03-04 20:27:06 +0300

    100'th commmit edit adnd delete branche endpoint

--- backend/system_admin/urls.py
+++ backend/system_admin/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import branchView,localAccountView
+from .views import branchView,localAccountView,branchDetail
 from . import views
 
 urlpatterns = [
     path('accounts/', localAccountView.as_view()),
     path('accounts/add/',views.addAccount),
     path('branch/', branchView.as_view()),
+    path('branch/<int:pk>/', branchDetail.as_view()),
     path('branch/add', views.addBranch),
+
     
 
 ]
