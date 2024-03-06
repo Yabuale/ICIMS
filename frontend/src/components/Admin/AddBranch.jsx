eba826151dfdead601ce9e92d7acb4e939846b0a
Yeabsra Alebachew <yeabsraalebachew9@gmail.com>
2024-03-06 21:39:24 +0300

    add branch complited

--- frontend/src/components/Admin/AddBranch.jsx
+++ frontend/src/components/Admin/AddBranch.jsx
@@ -1,100 +1,155 @@
-import React from 'react';
+import axios from 'axios'
+import { useState } from 'react';
+import ConfModal from '../conformModal';
+import ErrorModal from '../errorModal';
+import SuccessModal from '../successModal';
 
-const AddBranch = () => {
-    return (
-        <>
-            <div className="bg-sky-50 border rounded-lg px-8 py-6 mx-auto my-8 max-w-2xl">
-            <div class="flex justify-center py-4">
-           <div class="flex bg-gray-50 rounded-full md:p-4 p-2 border-2 border-sky-800">
-           <svg
-                   className="w-12 text-sky-900"
-                   viewBox="0 0 24 24"
-                   strokeLinejoin="round"
-                   strokeWidth="2"
-                   strokeLinecap="round"
-                   strokeMiterlimit="10"
-                   stroke="currentColor"
-                   fill="none"
-                 >
-                   <rect x="3" y="1" width="7" height="12" />
-                   <rect x="3" y="17" width="7" height="6" />
-                   <rect x="14" y="1" width="7" height="6" />
-                   <rect x="14" y="11" width="7" height="12" />
-                 </svg>
-           </div>
-         </div>
-     
-         <div class="flex justify-center">
-           <div class="flex">
-             <h1 class="text-gray-600 font-bold md:text-2xl text-xl">Add Branch</h1>
-           </div>
-         </div>
-                <form>
-                    <div className="mb-4">
-                        <label htmlFor="name" className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Branch Name</label>
-                        <input type="text" id="name" name="name" placeholder="Branch"
-                            className="border border-gray-400 p-2 w-full rounded-lg focus:outline-none focus:border-blue-400" required />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="gender" className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Region</label>
-                        <select id="gender" name="Region"
-                            className="border border-gray-400 p-2 w-full rounded-lg focus:outline-none focus:border-blue-400" required>
-                            <option value="">Region</option>
-                            <option value="Addis Ababa">Addis Ababa</option>
-                            <option value="Afar Region">Afar Region</option>
-                            <option value="Amhara Region">Amhara Region</option>
-                            <option value="Amhara Region">Benishangul-Gumuz Region</option>
-                            <option value="Amhara Region">Central Ethiopia Region</option>
-                            <option value="Amhara Region">Dire Dawa</option>
-                            <option value="Amhara Region">Gambela Region</option>
-                            <option value="Amhara Region">Harari Region</option>
-                            <option value="Amhara Region">Oromia Region</option>
-                            <option value="Amhara Region">Sidama Region</option>
-                            <option value="Amhara Region">South Ethiopia Region</option>
-                            <option value="Amhara Region">Southwest Ethiopia Peoples’ Region</option>
-                            <option value="Amhara Region">Tigray Region</option>
-                        </select>
-                    </div>
-                    
-                    <div class="mb-4">
-                <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">
-                    Address Details
-                </label>
-                <div class="-mx-3 flex flex-wrap">
-                    <div class="w-full px-3 sm:w-1/2">
-                        <div class="mb-5">
-                            <input type="text" name="area" id="area" placeholder="Enter Zone"
-                                class="w-full md:text-sm rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                        </div>
-                    </div>
-                    <div class="w-full px-3 sm:w-1/2">
-                        <div class="mb-5">
-                            <input type="text" name="city" id="city" placeholder="Enter city"
-                                class="w-full md:text-sm rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-semibold text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                        </div>
-                    </div>
-                    <div class="w-full px-3 sm:w-1/2">
-                        <div class="mb-5">
-                            <input type="text" name="state" id="state" placeholder="Enter Woreda"
-                                class="w-full md:text-sm rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                        </div>
-                    </div>
-                    <div class="w-full px-3 sm:w-1/2">
-                        <div class="mb-5">
-                            <input type="text" name="post-code" id="post-code" placeholder="Kebele"
-                                class="w-full md:text-sm rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-                    
-                    <div>
-                        <button type="submit" className="bg-sky-800 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Submit</button>
-                    </div>
-                </form>
-            </div>
-        </>
-    );
+
+
+const AddBranch = () =>{
+ const [errStatus, setErrStatus]=useState('500')
+ const[errMsg, setErrMsg]=useState('The server might be down, please try again later we will try to solve the problem as soon as possible')
+ const[succMsg,setSuccMsg]=useState("sdfsdfsdfsdfsdfsdfsdf")
+ const [branch_name, setBranch_name] = useState('')
+ const [woreda, setWoreda] = useState('')
+ const [zone, setZone]= useState('')
+ const [region, setRegion]= useState('')
+ const [phone_no, setPhone_no]= useState('')
+ const [eamil_address, setEamil_address]= useState('')
+ 
+ const [showconfModal, showconformModal] = useState(false);
+ const [showerrModal,showerrorModal] = useState(false)
+ const [showsccModal, showsuccessModal] = useState(false)
+ 
+const url = 'http://127.0.0.1:8000/systemadmin/branch/add'
+ const handleSubmit = async (e) =>{
+  showconformModal(false)
+  e.preventDefault();
+   try{
+  const resp =await axios.post(url, {branch_name:branch_name,woreda:woreda,zone:zone,region:region,phone_no:phone_no,eamil_address:eamil_address});
+  if(resp.status === 201){
+    setSuccMsg(resp.data.success)
+    showsuccessModal(true)
+  }
+   } catch(error){
+    showerrorModal(true)
+   if(error.response.status === 400 ){
+    setErrStatus(error.response.status);
+    setErrMsg(error.response.data.success);
+     }
+    
+  }
+ } 
+ const handleOpenModal = () => {
+  showconformModal(true); // Open the modal on button click
 };
+
+return( 
+ <>
+ 
+ <br/>
+ <br/>
+ <br/>
+
+
+  <div className="grid  mx-24 lg:mx-auto   bg-sky-50   rounded-lg shadow-xl w-4/5  md:w-9/12 lg:w-1/2">
+    <div className="flex justify-center py-4">
+      <div className="flex bg-gray-50 rounded-full md:p-4 p-2 border-2 border-sky-800">
+      <svg
+              className="w-12 text-sky-900"
+              viewBox="0 0 24 24"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeMiterlimit="10"
+              stroke="currentColor"
+              fill="none"
+            >
+              <rect x="3" y="1" width="7" height="12" />
+              <rect x="3" y="17" width="7" height="6" />
+              <rect x="14" y="1" width="7" height="6" />
+              <rect x="14" y="11" width="7" height="12" />
+            </svg>
+      </div>
+    </div>
+
+    <div className="flex justify-center">
+      <div className="flex">
+        <h1 className="text-gray-600 font-bold md:text-2xl text-xl">Add Branch</h1>
+      </div>
+    </div>
+     <form onSubmit={handleSubmit}>
+    <div className="grid grid-cols-1 mt-5 mx-7">
+      <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Branch Name</label>
+      <input className="py-2 px-3 rounded-lg border-2 border-sky-300 mt-1 focus:outline-none focus:ring-2 focus:ring-sky-600 focus:border-transparent" type="text" placeholder="branch name" value={branch_name} id="branch_name" onChange={(e)=> setBranch_name(e.target.value)} />
+    </div>
+
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">
+      <div className="grid grid-cols-1">
+        <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Woreda</label>
+        <input className="py-2 px-3 rounded-lg border-2 border-sky-300 mt-1 focus:outline-none focus:ring-2 focus:ring-sky-600 focus:border-transparent" type="text" placeholder="woreda"  value={woreda} id="woreda" onChange={(e)=> setWoreda(e.target.value)}/>
+      </div>
+      <div className="grid grid-cols-1">
+      <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">region</label>
+      <select className="py-2 px-3 rounded-lg border-2 border-sky-300 mt-1 focus:outline-none focus:ring-2 focus:ring-sky-600 focus:border-transparent" value={region} id="region" onChange={(e)=> setRegion(e.target.value)}>
+        
+      <option> </option>
+      <option value="afar">Afar</option>
+      <option value="amhara">Amhara</option>
+      <option value="benishangul-gumuz">Benishangul-Gumuz</option>
+       <option value="gambella">Gambella</option>
+      <option value="harari">Harari</option>
+        <option value="oromia">Oromia</option>
+       <option value="somali">Somali</option>
+       <option value="southern-nations">Southern Nations, Nationalities, and Peoples' Region</option>
+       <option value="tigray">Tigray</option>
+        
+      </select>
+    </div>
+    </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">
+      <div className="grid grid-cols-1">
+        <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Zone</label>
+        <input className="py-2 px-3 rounded-lg border-2 border-sky-300 mt-1 focus:outline-none focus:ring-2 focus:ring-sky-600 focus:border-transparent" type="text" placeholder="zone" value={zone} id="zone" onChange={(e)=> setZone(e.target.value)} />
+      </div>
+      <div className="grid grid-cols-1">
+        <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">phone_no</label>
+        <input className="py-2 px-3 rounded-lg border-2 border-sky-300 mt-1 focus:outline-none focus:ring-2 focus:ring-sky-600 focus:border-transparent" type="text" placeholder="phone_no" value={phone_no} id="phone_no" onChange={(e)=> setPhone_no(e.target.value)} />
+      </div>
+    </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">
+      <div className="grid grid-cols-1">
+        <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">eamil address</label>
+        <input className="py-2 px-3 rounded-lg border-2 border-sky-300 mt-1 focus:outline-none focus:ring-2 focus:ring-sky-600 focus:border-transparent" type="email" placeholder="example@gmail.com" value={eamil_address} id="eamil_address" onChange={(e)=> setEamil_address(e.target.value)} />
+      </div>
+      
+    </div>
+
+   
+
+    
+
+    
+
+    <div className='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
+      <button className='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Cancel</button>
+      <div>
+      <button className='w-auto bg-sky-500 hover:bg-sky-700 rounded-lg shadow-xl font-medium text-white px-4 py-2' type='button' onClick={handleOpenModal}>Add</button>
+      {showconfModal && <ConfModal showconfModal={showconfModal} message={'you are about to create a branch please conform by clicking post'} clickbutton={'Add'} onClose={() => showconformModal(false)} />}
+      {showerrModal && <ErrorModal showerrModal={showerrModal} errMsg={errMsg} errStatus={errStatus} onClose={() => showerrorModal(false)} />}
+      {showsccModal && <SuccessModal showsccModal={showsccModal} succMsg={succMsg} onClose={() => showsuccessModal(false)} />}
+      </div>
+    </div>
+    </form>
+  </div>
+
+  <br/>
+  
+
+ </>
+
+);
+
+}
 
 export default AddBranch;
